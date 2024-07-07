55c3b0bb357236fd3ba3b46c5517dd0955eb6715
arcstur <arcstur@gmail.com>
2024-07-07 12:35:22 -0300

    feat: add language and total phoneme count in language list page

--- src/languages/templates.rs
+++ src/languages/templates.rs
@@ -9,6 +9,19 @@ pub(super) struct List {
     pub(super) languages: Vec<Language>,
 }
 
+impl List {
+    fn language_count(&self) -> usize {
+        self.languages.iter().count()
+    }
+
+    fn total_phoneme_count(&self) -> u32 {
+        self.languages
+            .iter()
+            .map(|l| l.phoneme_count.value.parse::<u32>().unwrap_or_default())
+            .sum()
+    }
+}
+
 #[derive(Template)]
 #[template(path = "languages/details.html")]
 pub(super) struct Details {
