51aef05b9b030917858005ca074b8138dc1587d0
arcstur <arcstur@gmail.com>
2024-07-07 20:19:15 -0300

    refactor: move obelepedia languages into its own module

--- src/activity/mod.rs
+++ src/activity/mod.rs
@@ -1,8 +1,10 @@
+mod obelepedia;
 mod templates;
 
 use axum::routing::get;
 
 use crate::AppRouter;
+use obelepedia::OBELEPEDIA;
 use templates::List;
 
 pub fn router() -> AppRouter {
@@ -19,17 +21,3 @@ struct Language {
     en_label: &'static str,
     pt_wikipedia: &'static str,
 }
-
-const OBELEPEDIA: [Language; 2] = [
-    Language {
-        qid: "Q21117",
-        en_label: "Central Alaskan Yup'ik",
-        pt_wikipedia: "https://pt.wikipedia.org/w/index.php?title=L%C3%ADngua_i%C3%BApique_do_Alasca_Central&oldid=67946266",
-    },
-    Language {
-        qid: "Q4023175",
-        en_label: "Jurúna",
-        pt_wikipedia:
-            "https://pt.wikipedia.org/w/index.php?title=L%C3%ADngua_yudj%C3%A1&oldid=67386586",
-    },
-];
---
+++ src/activity/obelepedia.rs
@@ -0,0 +1,19 @@
+use super::Language;
+
+pub(super) const OBELEPEDIA: [Language; 3] = [
+    Language {
+        qid: "Q21117",
+        en_label: "Central Alaskan Yup'ik",
+        pt_wikipedia: "https://pt.wikipedia.org/wiki/L%C3%ADngua_i%C3%BApique_do_Alasca_Central",
+    },
+    Language {
+        qid: "Q4023175",
+        en_label: "Jurúna",
+        pt_wikipedia: "https://pt.wikipedia.org/wiki/L%C3%ADngua_yudj%C3%A1",
+    },
+    Language {
+        qid: "Q8785",
+        en_label: "Armenian",
+        pt_wikipedia: "https://pt.wikipedia.org/wiki/L%C3%ADngua_arm%C3%AAnia",
+    },
+];
