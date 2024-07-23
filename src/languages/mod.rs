0271908252f4a6d94619a5a81d68d362aa361452
arcstur <arcstur@gmail.com>
2024-07-23 18:41:45 -0300

    feat: implement locking and finishing

--- src/languages/mod.rs
+++ src/languages/mod.rs
@@ -4,6 +4,8 @@ use axum::{
     extract::{Form, Path, State},
     routing::{get, post},
 };
+use axum_login::AuthUser;
+use sqlx::SqlitePool;
 
 use crate::{
     api::{AddPhonemeInput, Editor},
@@ -11,7 +13,7 @@ use crate::{
     AppRouter, Client, EntityId, Result, User, WikiValue,
 };
 use serde::Deserialize;
-use templates::{List, PhonemeAdded};
+use templates::{AvailablePhonemes, List, PhonemeAdded, Status};
 
 use self::templates::Details;
 
@@ -19,6 +21,12 @@ pub fn router() -> AppRouter {
     AppRouter::new()
         .route("/", get(list_languages))
         .route("/:id", get(single_language))
+        .route("/:id/mark_as_working", post(mark_as_working))
+        .route("/:id/unmark_as_working", post(unmark_as_working))
+        .route("/:id/finish", post(finish))
+        .route("/:id/undo_finish", post(undo_finish))
+        .route("/:id/available_phonemes", get(available_phonemes))
+        .route("/:id/status", get(status))
         .route("/:id/add_phoneme", post(add_phoneme))
 }
 
@@ -27,29 +35,16 @@ async fn list_languages(State(client): State<Client>) -> Result<List> {
     Ok(List { languages })
 }
 
-async fn single_language(
-    user: Option<User>,
-    State(client): State<Client>,
-    Path(id): Path<String>,
-) -> Result<Details> {
+async fn single_language(State(client): State<Client>, Path(id): Path<String>) -> Result<Details> {
     let id = EntityId(id);
     let phonemes = Phoneme::by_language(&client, &id).await?;
     let label_or_id = client
         .english_label(&id)
         .await?
         .unwrap_or(format!("Language {id}"));
-    let is_logged_in = user.is_some();
-
-    let available_phonemes = if is_logged_in {
-        Phoneme::by_language_opposite(&client, &id).await?
-    } else {
-        Vec::new()
-    };
 
     Ok(Details {
-        is_logged_in,
         phonemes,
-        available_phonemes,
         label_or_id,
         id,
     })
@@ -85,6 +80,127 @@ async fn add_phoneme(
         .await?;
 
     Ok(PhonemeAdded {})
+}
+
+async fn available_phonemes(
+    State(client): State<Client>,
+    _user: User,
+    Path(id): Path<String>,
+) -> Result<AvailablePhonemes> {
+    let id = EntityId(id);
+    let label_or_id = client
+        .english_label(&id)
+        .await?
+        .unwrap_or(format!("Language {id}"));
+    let available_phonemes = Phoneme::by_language_opposite(&client, &id).await?;
+
+    Ok(AvailablePhonemes {
+        id,
+        available_phonemes,
+        label_or_id,
+    })
+}
+
+async fn status(
+    State(pool): State<SqlitePool>,
+    user: Option<User>,
+    Path(qid): Path<String>,
+) -> Result<Status> {
+    Status::generate(&pool, user, qid).await
+}
+
+impl Status {
+    async fn generate(pool: &SqlitePool, user: Option<User>, qid: String) -> Result<Self> {
+        let status = sqlx::query_as!(
+            StatusQuery,
+            "
+            SELECT
+                s.is_finished,
+                u.username
+            FROM language_status s
+            LEFT JOIN users u ON (u.id = s.working_user)
+            WHERE s.qid = ?
+            ",
+            qid,
+        )
+        .fetch_optional(pool)
+        .await?
+        .unwrap_or_default();
+
+        let id = EntityId(qid);
+        let working_user_username = status.username.unwrap_or_default();
+        let is_finished = status.is_finished;
+
+        Ok(Status {
+            user,
+            id,
+            is_finished,
+            working_user_username,
+        })
+    }
+}
+
+#[derive(Default)]
+struct StatusQuery {
+    is_finished: bool,
+    username: Option<String>,
+}
+
+async fn mark_as_working(
+    State(pool): State<SqlitePool>,
+    user: User,
+    Path(qid): Path<String>,
+) -> Result<Status> {
+    let user_id = user.id();
+
+    sqlx::query!(
+        "REPLACE INTO language_status ('qid', 'working_user') VALUES (?, ?)",
+        qid,
+        user_id,
+    )
+    .execute(&pool)
+    .await?;
+
+    Status::generate(&pool, Some(user), qid).await
+}
+
+async fn unmark_as_working(
+    State(pool): State<SqlitePool>,
+    user: User,
+    Path(qid): Path<String>,
+) -> Result<Status> {
+    sqlx::query!("DELETE FROM language_status WHERE qid = ?", qid)
+        .execute(&pool)
+        .await?;
+    Status::generate(&pool, Some(user), qid).await
+}
+
+async fn finish(
+    user: User,
+    State(pool): State<SqlitePool>,
+    Path(qid): Path<String>,
+) -> Result<Status> {
+    sqlx::query!(
+        "UPDATE language_status SET is_finished = true WHERE qid = ?",
+        qid,
+    )
+    .execute(&pool)
+    .await?;
+    Status::generate(&pool, Some(user), qid).await
+}
+
+async fn undo_finish(
+    user: User,
+    State(pool): State<SqlitePool>,
+    Path(qid): Path<String>,
+) -> Result<Status> {
+    sqlx::query!(
+        "UPDATE language_status SET is_finished = false WHERE qid = ?",
+        qid,
+    )
+    .execute(&pool)
+    .await?;
+    Status::generate(&pool, Some(user), qid).await
 }
 
 #[derive(Debug, Deserialize)]
--- src/languages/templates.rs
+++ src/languages/templates.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 
 use super::Language;
-use crate::{EntityId, Phoneme};
+use crate::{EntityId, Phoneme, User};
 
 #[derive(Template)]
 #[template(path = "languages/list.html")]
@@ -25,11 +25,26 @@ impl List {
 #[derive(Template)]
 #[template(path = "languages/details.html")]
 pub(super) struct Details {
-    pub(super) is_logged_in: bool,
     pub(super) phonemes: Vec<Phoneme>,
-    pub(super) available_phonemes: Vec<Phoneme>,
     pub(super) label_or_id: String,
     pub(super) id: EntityId,
+}
+
+#[derive(Template)]
+#[template(path = "languages/status.html")]
+pub(super) struct Status {
+    pub(super) user: Option<User>,
+    pub(super) id: EntityId,
+    pub(super) is_finished: bool,
+    pub(super) working_user_username: String,
+}
+
+#[derive(Template)]
+#[template(path = "languages/available_phonemes.html")]
+pub(super) struct AvailablePhonemes {
+    pub(super) available_phonemes: Vec<Phoneme>,
+    pub(super) id: EntityId,
+    pub(super) label_or_id: String,
 }
 
 #[derive(Template)]
