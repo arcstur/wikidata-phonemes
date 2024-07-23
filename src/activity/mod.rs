643e9fc773068253693a3c9e6f0784ee6e4bfea5
arcstur <arcstur@gmail.com>
2024-07-23 12:56:14 -0300

    refactor: use new db table in activity language list

--- src/activity/mod.rs
+++ src/activity/mod.rs
@@ -1,29 +1,48 @@
-mod obelepedia;
 mod templates;
-mod working_languages;
 
-use axum::routing::get;
-use working_languages::WORKING_LANGUAGES;
+use axum::{extract::State, routing::get};
+use sqlx::SqlitePool;
 
-use crate::AppRouter;
-use obelepedia::OBELEPEDIA;
+use crate::{AppRouter, Result};
 use templates::List;
 
 pub fn router() -> AppRouter {
     AppRouter::new().route("/", get(languages_list))
 }
 
-async fn languages_list() -> List {
-    let obelepedia_languages = OBELEPEDIA.into();
-    let working_languages = WORKING_LANGUAGES.into();
-    List {
-        obelepedia_languages,
-        working_languages,
-    }
+async fn languages_list(State(pool): State<SqlitePool>) -> Result<List> {
+    let languages = sqlx::query!(
+        "
+        SELECT
+            l.qid,
+            l.en_label,
+            l.wikipedia_url,
+            s.is_finished,
+            COALESCE(wu.username, '') AS working_user_username
+        FROM languages l
+        LEFT JOIN language_status s ON (s.qid = l.qid)
+        LEFT JOIN users wu ON (s.working_user = wu.id)
+        "
+    )
+    .fetch_all(&pool)
+    .await?
+    .into_iter()
+    .map(|r| Language {
+        qid: r.qid,
+        en_label: r.en_label,
+        wikipedia_url: r.wikipedia_url,
+        is_finished: r.is_finished.unwrap_or(false),
+        working_user_username: r.working_user_username,
+    })
+    .collect();
+
+    Ok(List { languages })
 }
 
 struct Language {
-    qid: &'static str,
-    en_label: &'static str,
-    wikipedia_url: &'static str,
+    qid: String,
+    en_label: String,
+    wikipedia_url: String,
+    is_finished: bool,
+    working_user_username: String,
 }
--- src/activity/templates.rs
+++ src/activity/templates.rs
@@ -4,6 +4,5 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "activity/list.html")]
 pub(super) struct List {
-    pub(super) obelepedia_languages: Vec<Language>,
-    pub(super) working_languages: Vec<Language>,
+    pub(super) languages: Vec<Language>,
 }
